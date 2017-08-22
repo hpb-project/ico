4122adcdd29078d9370a757afa84d2bcf5a0cf96
wangxiaoming <me@wangxiaoming.com>
2017-08-22 11:20:11 +0800

    [Change] test/TestHPBToken.js

--- test/TestHPBToken.js
+++ test/TestHPBToken.js
@@ -3,20 +3,20 @@ var HPBToken = artifacts.require("HPBToken");
 contract('HPBToken', function(accounts) {
   it("should not be able to start ico sale when not called by owner.", function() {
     console.log("\n" + "-".repeat(100) + "\n");
-    var loopring;
+    var hbp;
     var target;
     return HPBToken.deployed().then(function(instance) {
-      loopring = instance;
-      console.log("loopring:", loopring.address);
-      return loopring.target.call({from: accounts[1]});
+      hbp = instance;
+      console.log("hbp:", hbp.address);
+      return hbp.target.call({from: accounts[1]});
     }).then(function(t){
       target = accounts[1];
-      web3.eth.sendTransaction({from: accounts[1], to: loopring.address, value: web3.toWei(1) })
+      web3.eth.sendTransaction({from: accounts[1], to: hbp.address, value: web3.toWei(1) })
       return web3.eth.sendTransaction({from: accounts[1], to: target, value: web3.toWei(1) })
     }).then(function(tx) {
       console.log("tx:", tx);
       console.log("blockNumber:", web3.eth.blockNumber);
-      return loopring.start(100, {from: accounts[1]});
+      return hbp.start(100, {from: accounts[1]});
     }).then(function(tx) {
       console.log("tx:", tx);
       if (tx.logs) {
@@ -35,16 +35,16 @@ contract('HPBToken', function(accounts) {
 
   it("should not allow create tokens before it starts", function() {
     console.log("\n" + "-".repeat(100) + "\n");
-    var loopring;
+    var hbp;
     var target;
     return HPBToken.deployed().then(function(instance) {
-      loopring = instance;
-      return loopring.target.call({from: accounts[1]});
+      hbp = instance;
+      return hbp.target.call({from: accounts[1]});
     }).then(function(t){
       target = t;
       console.log("target:", target);
       firstblock = t.blockNumber + 1;
-      return web3.eth.sendTransaction({from: accounts[1], to: loopring.address, value: web3.toWei(1) });
+      return web3.eth.sendTransaction({from: accounts[1], to: hbp.address, value: web3.toWei(1) });
 
     }).then(function(txHash) {
       console.log("txHash", txHash);
@@ -57,14 +57,14 @@ contract('HPBToken', function(accounts) {
 
   it("should be able to start ico sale when called by owner.", function() {
     console.log("\n" + "-".repeat(100) + "\n");
-    var loopring;
+    var hbp;
     var target;
     return HPBToken.deployed().then(function(instance) {
-      loopring = instance;
-      return loopring.target.call();
+      hbp = instance;
+      return hbp.target.call();
     }).then(function(t){
       target = t;
-      return loopring.start(10, {from: target});
+      return hbp.start(10, {from: target});
     }).then(function(tx) {
       console.log("tx: ", tx);
       if (tx.logs) {
@@ -83,18 +83,18 @@ contract('HPBToken', function(accounts) {
 
   it("should be able to create HPB tokens after sale starts", function() {
     console.log("\n" + "-".repeat(100) + "\n");
-    var loopring;
+    var hbp;
     var target;
     var externalTxHash;
     return HPBToken.deployed().then(function(instance) {
-      loopring = instance;
-      return loopring.target.call({from: accounts[1]});
+      hbp = instance;
+      return hbp.target.call({from: accounts[1]});
     }).then(function(t){
       target = t;
-      return web3.eth.sendTransaction({from: accounts[1], to: loopring.address, value: web3.toWei(1), gas: 500000 });
+      return web3.eth.sendTransaction({from: accounts[1], to: hbp.address, value: web3.toWei(1), gas: 500000 });
     }).then(function(tx) {
       console.log("tx:", tx);
-      return loopring.balanceOf(accounts[1], {from: accounts[1]});
+      return hbp.balanceOf(accounts[1], {from: accounts[1]});
     }).then(function(bal) {
       console.log("bal: ", bal.toNumber());
       assert.equal(bal.toNumber(), web3.toWei(6000), "no HPB token Transfer event found");
